11564c51b9c9ac475fd537e524c2d081a0ceb2d4
Peter Thompson <peter@petethompson.net>
2021-12-01 21:49:18 +0100

    Add day 1 part 1

--- lib/advent/day_01.ex
+++ lib/advent/day_01.ex
@@ -1,7 +1,14 @@
 defmodule Advent.Day01 do
   def part1(input) do
-    "hello"
+    String.split(input)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce({nil, 0}, &increase_check/2)
+    |> elem(1)
   end
+
+  defp increase_check(n, {nil, 0}), do: {n, 0}
+  defp increase_check(n, {prev, count}) when n > prev, do: {n, count + 1}
+  defp increase_check(n, {_prev, count}), do: {n, count}
 
   def part2(_input) do
     "world"
