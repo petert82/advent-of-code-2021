f055553b2ef670db28b9e663522fa6b065f1c3ce
Peter Thompson <peter@petethompson.net>
2021-12-02 20:18:33 +0100

    Add day 1 part 2

--- lib/advent/day_01.ex
+++ lib/advent/day_01.ex
@@ -10,7 +10,12 @@ defmodule Advent.Day01 do
   defp increase_check(n, {prev, count}) when n > prev, do: {n, count + 1}
   defp increase_check(n, {_prev, count}), do: {n, count}
 
-  def part2(_input) do
-    "world"
+  def part2(input) do
+    String.split(input)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.reduce({nil, 0}, &increase_check/2)
+    |> elem(1)
   end
 end
--- test/advent/day_01_test.exs
+++ test/advent/day_01_test.exs
@@ -21,4 +21,22 @@ defmodule Advent.Day01Test do
 
     assert part1(input) == 7
   end
+
+  @tag :day01
+  test "part2" do
+    input = """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    """
+
+    assert part2(input) == 5
+  end
 end
